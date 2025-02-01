987aef87f145c4909199669aaa7369fcd788aed6
Sondre Wold <sondrewold1@gmail.com>
2025-02-01 19:48:36 +0100

    return summaries

--- backend/generate_summary.py
+++ backend/generate_summary.py
@@ -16,5 +16,21 @@ def generate_response(text_to_summarize: str) -> str:
     return raw["response"]
 
 
+def generate_meta_response(text_to_summarize: str) -> str:
+    prompt: str = f"Aggregate the following summaries into a shared json object \n{text_to_summarize}"
+
+    url = "http://localhost:11434/api/generate"
+    payload = {
+        "model": "knut",
+        "prompt": prompt,
+        "stream": False
+    }
+    headers = {"Content-Type": "application/json"}
+
+    raw = requests.post(url, json=payload, headers=headers).json()
+
+    return raw["response"]
+
+
 if __name__ == "__main__":
     print(generate_response("This is a text about everything possible!"))
