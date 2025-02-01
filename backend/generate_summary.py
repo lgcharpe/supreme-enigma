1172efb47450d5e06fb6c0a03cb95faed7950cd9
Lucas Georges Gabriel Charpentier Pacheco <lgcharpe@gmail.com>
2025-02-01 16:04:28 +0100

    Adding v1 of the generating summary function

---
+++ backend/generate_summary.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def generate_response(text_to_summarize: str) -> str:
+    prompt: str = f"Kan du oppsummere: \n{text_to_summarize}"
+
+    url = "http://localhost:11434/api/generate"
+    payload = {
+        "model": "ola",
+        "prompt": prompt,
+        "stream": False
+    }
+    headers = {"Content-Type": "application/json"}
+
+    raw = requests.post(url, json=payload, headers=headers)
+
+    print(raw["response"])
+
+
+if __name__ == "__main__":
+    generate_response("This is a text about everything possible!")
